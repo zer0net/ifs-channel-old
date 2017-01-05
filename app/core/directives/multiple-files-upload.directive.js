b7b2cce4ffac1739c940211352de436579bcd1a7
david nelband <dnelband@gmail.com>
2017-01-05 13:04:31 +0100

    fix special chars optional files bug

--- app/core/directives/multiple-files-upload.directive.js
+++ app/core/directives/multiple-files-upload.directive.js
@@ -62,79 +62,82 @@ app.directive('multipleFilesUpload', ['$location',
 			};
 
 			
-
 			// upload single file
 			$scope.uploadFile = function(file){
-				
-				if($scope.ifFileExist(file))
-				{
+				console.log(file);
+				if ($scope.ifFileExist(file)) {
 					file.state = 'existing';
 					$scope.uploadNextFile();
-						
-				}else
-				{
+				} else {
 					// file state
 					file.state = 'uploading';
-					    	// get items file type
-							var splitByLastDot = function(text) {
-							    var index = text.lastIndexOf('.');
-							    return [text.slice(0, index), text.slice(index + 1)]
-							}
-							var file_type = splitByLastDot(file.name)[1];
+			    	// get items file type
+					var splitByLastDot = function(text) {
+					    var index = text.lastIndexOf('.');
+					    return [text.slice(0, index), text.slice(index + 1)]
+					}
+					var file_type = splitByLastDot(file.name)[1];
+					var file_name = file.name.split(' ').join('_').normalize('NFKD').replace(/[\u0300-\u036F]/g, '').replace(/ß/g,"ss").split('.' + file_type)[0].replace(/[^\w\s]/gi, '') + '.' + file_type;
 
-							// item obj
-							var item = {
-								"file_type":file_type,
-								"channel": $location.$$absUrl.split('0/')[1].split('/')[0],
-								"title": file.name.split('.'+file_type)[0],
-								"date_added": +(new Date),
-								"published":false
-							};
+					// item obj
+					var item = {
+						"file_type":file_type,
+						"channel": $location.$$absUrl.split('0/')[1].split('/')[0],
+						"title": file.name.split('.'+file_type)[0],
+						"date_added": +(new Date),
+						"published":false
+					};
 
-							// render item
-							var item_id_name;
-							var item_file_name;
-							if (file_type === 'zip') {
-								item_id_name = 'game_id';
-								item.zip_size = file.size;
-								item_file_name = 'zip_name';														
-								item.media_type = 'game';
-								item.file_name=file.name;
-								item.file_size=file.size;
-								item.path = 'uploads/games/'+file.name;
-							} else if (file_type === 'nes') {
-								item_id_name = 'game_id';
-								item_file_name = 'file_name';						
-								item.file_size = file.size;
-								item.media_type = 'game';
-								item.path = 'uploads/games/'+file.name;
-							} else {
-								item_id_name = 'video_id';
-								item_file_name = 'file_name';						
-								item.media_type = 'video';
-								item.file_size = file.size;
-								item.path = 'uploads/videos/'+file.name;						
-							}
+					// render item
+					var item_id_name;
+					var item_file_name;
+					if (file_type === 'zip') {
+						item_id_name = 'game_id';
+						item.zip_size = file.size;
+						item_file_name = 'zip_name';														
+						item.media_type = 'game';
+						item.file_name=file_name;
+						item.file_size=file.size;
+						item.path = 'uploads/games/'+file_name;
+					} else if (file_type === 'nes') {
+						item_id_name = 'game_id';
+						item_file_name = 'file_name';						
+						item.file_size = file.size;
+						item.media_type = 'game';
+						item.path = 'uploads/games/'+file_name;
+					} else {
+						item_id_name = 'video_id';
+						item_file_name = 'file_name';						
+						item.media_type = 'video';
+						item.file_size = file.size;
+						item.path = 'uploads/videos/'+file_name;						
+					}
 
-							// item file name
-							item[item_file_name] = file.name;
-							// item id
-							item[item_id_name] = $scope.chJson.next_item_id;
-
-							// write to file
-							Page.cmd("fileWrite",[item.path, file.data.split('base64,')[1]], function(res) {
-								$scope.$apply(function(){
-									// file state
-									file.state = 'done!';
-									// item id update
-									$scope.chJson.next_item_id += 1;
-									// push item to channel json items
-									var media_type = item.media_type + 's';
-									$scope.chJson[media_type].push(item);
-									// upload next file
-									$scope.uploadNextFile();
-								});
-							});
+					// item file name
+					item[item_file_name] = file_name;
+					// item id
+					var next_item_id;
+					if (!$scope.chJson){
+						next_item_id = 1;
+					} else {
+						next_item_id = $scope.chJson.next_item_id;
+					}
+					item[item_id_name] = next_item_id;
+					console.log(item);
+					// write to file
+					Page.cmd("fileWrite",[item.path, file.data.split('base64,')[1]], function(res) {
+						$scope.$apply(function(){
+							// file state
+							file.state = 'done!';
+							// item id update
+							$scope.chJson.next_item_id += 1;
+							// push item to channel json items
+							var media_type = item.media_type + 's';
+							$scope.chJson[media_type].push(item);
+							// upload next file
+							$scope.uploadNextFile();
+						});
+					});
 				}
 				
 				
